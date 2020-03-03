175d47a909f52a1a4d6a1a78021a5c87174d68bf
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2020-03-02 22:06:57 -0500

    Add files via upload

---
+++ participation_08.c
@@ -0,0 +1,52 @@
+//Farhan Shahbaz
+
+//Williamson
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+{
+    
+	int width;
+    
+	int* wPtr = &width;
+    
+	printf("Please enter the width of the room:\n");
+    
+	scanf("%d", wPtr);
+    
+	//printf("Width is: %d\n", *wPtr);
+    
+    
+
+	int length;
+    
+	int* lPtr = &length;
+    
+	printf("Please enter the lenght of the room\n");
+    
+	scanf("%d", lPtr);
+    
+	//printf("Length is %d\n", *lPtr);
+    
+    
+
+	int area;
+    
+	area = (*lPtr) * (*wPtr);
+    
+	int* aPtr = &area;
+    
+    
+	
+	printf("Area of a room with dimensions %d x %d is: %d ft^2\n", *wPtr, *lPtr, *aPtr);
+   
+    
+
+	return 0;
+
+}
